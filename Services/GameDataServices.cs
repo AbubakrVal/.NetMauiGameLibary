79ba48f34f6357ec838d27d1a52bed935a9ba7ab
AbubakrVal <Valentinemay445@gmail.com>
2025-07-17 13:35:46 +0200

    Refactor GameDataServices to improve UpdateGameInformation logic and ensure proper handling of game information retrieval by Id and Title

--- Services/GameDataServices.cs
+++ Services/GameDataServices.cs
@@ -9,13 +9,40 @@ namespace .NetMauiGameLibary.Services
 {
     public class GameDataServices
     {
-        public GameInformation GetgameInformationById(string Id)
+        private List<GameInformation> _gameInformation = new list<GameInformation>();
+
+
+        
+        public List<GameInformation> GetAllGameInformation()
         {
-            
+            return _gameInformation; 
         }
-        public GameInformation GetgameInformationByTitle(string Title)
+
+        public void UpdateGameInformation(GameInformation gameInformation)
         {
-            
+           if (!string.iNullorEmpty(gameInformation.Id))
+           {
+                //update
+                var uniqueGame = GetgameInformationById(gameInformation.Id);
+
+                int postion = _gameInformation.IndexOf(uniqueGame);
+
+                _gameInformation[postion] = gameInformation;
+            }
+            else
+            {
+                //insert
+                _gameInformation.Add(gameInformation)
+            }
+        }
+
+        public GameInformation GetgameInformationById(string id)
+        {
+            return _gameInformation.Where(gameInfo => gameInfo.Id == id).FirstOrDefault(); 
+        }
+        public List<GameInformation> GetgameInformationByTitle(string title)
+        {
+            return _gameInformation.Where(gameInfo => gameInfo.Title == title).FirstOrDefault(); 
         }
     }
 }
